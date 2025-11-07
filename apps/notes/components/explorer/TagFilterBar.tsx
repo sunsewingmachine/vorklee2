d4ba7d1711a6fdc109f2e223e22e4549f5d441ff
SunBathu <sunbathu@gmail.com>
2025-11-07 09:08:57 +0530

    62

--- apps/notes/components/explorer/TagFilterBar.tsx
+++ apps/notes/components/explorer/TagFilterBar.tsx
@@ -101,3 +101,4 @@ export function TagFilterBar({ selectedTagIds, onTagToggle, onClearFilters }: Ta
 
 
 
+
--- apps/notes/components/tags/TagSelector.tsx
+++ apps/notes/components/tags/TagSelector.tsx
@@ -131,3 +131,4 @@ export function TagSelector({ value, onChange, error, helperText }: TagSelectorP
 
 
 
+
